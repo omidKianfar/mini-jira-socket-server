4fdfc1afbe2e209cfcc4f59d1385fdb43d347ed6
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-25 16:11:54 +0330

    feat chat room 1 by 1 user for support

--- index.js
+++ index.js
@@ -3,15 +3,44 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 const app = express();
-
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: { origin: "*" },
 });
 
+const getChatId = (user1, user2) => [user1, user2].sort().join("_");
+
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
+
+  socket.on("chat:join", ({ userId, partnerId }) => {
+    const chatId = getChatId(userId, partnerId);
+
+    socket.join(chatId);
+
+    console.log(
+      `User ${userId} joined private chat with ${partnerId} (${chatId})`
+    );
+  });
+
+  socket.on("chat:send", ({ senderId, receiverId, text }) => {
+    const chatId = getChatId(senderId, receiverId);
+
+    io.to(chatId).emit("chat:receive", {
+      senderId,
+      receiverId,
+      text,
+      attachment: {
+        fileUrl: null,
+        fileType: null,
+      },
+      chatId,
+      timestamp: new Date().toISOString(),
+    });
+
+    console.log(`[${chatId}] ${senderId} → ${receiverId}: ${text}`);
+  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
@@ -19,5 +48,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT || 8080, () => {
-  console.log("Socket server running...");
+  console.log("Private Chat Socket server running...");
 });
